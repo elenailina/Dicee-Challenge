b2b9ae76285798dd6bfa7f7826b969b53a2bcdab
Elena <elenailina91@yahoo.com>
2022-01-26 17:55:26 +0300

    True

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function randomNumber() {
 img1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
 img2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-if (randomNumber1 < randomNumber2) {
+if (randomNumber1 > randomNumber2) {
     h1.innerHTML = "🚩Player 1 Wins!";
 } 
-else if (randomNumber1 > randomNumber2) {
+else if (randomNumber1 < randomNumber2) {
     h1.innerHTML = "Player 2 Wins!🚩";
 } else {
     h1.innerHTML = "Draw!";
